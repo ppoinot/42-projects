309efc2a991fd7df71d977b1976d00ca8fb29644
Paul-aurian POINOT <ppoinot@e3r11p18.42.fr>
2016-01-04 15:21:34 +0100

    040116 - 1521

--- fillit/check.c
+++ fillit/check.c
@@ -6,11 +6,23 @@
 /*   By: ppoinot <ppoinot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 04:47:27 by ppoinot           #+#    #+#             */
-/*   Updated: 2015/12/10 04:47:31 by ppoinot          ###   ########.fr       */
+/*   Updated: 2016/01/04 15:20:45 by ppoinot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define BUF_SIZE 16384
+
 #include "libft.h"
+
+static char		*add_tetris(int fd)
+{
+    int			oct_read;
+	char		*sample;
+
+	oct_read = read(fd, sample, BUF_SIZE);
+	if (oct_read == -1)
+		return (NULL);
+}
 
 int		main(void)
 {
@@ -20,6 +32,7 @@ void
 	fd = open("sample.fillit", O_RDONLY);
 	if (fd != -1)
 	{
+		char* = add_tetris(fd);
 		if (close(fd) == -1)
 		{
 			ft_putstr("close() error");
