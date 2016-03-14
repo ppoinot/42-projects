903411ea85438b4f14a1b40fd872d05eeac9659b
Paul-aurian POINOT <ppoinot@e1r7p1.42.fr>
2016-03-15 00:01:51 +0100

    mardi 15 mars 2016

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,25 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ppoinot <ppoinot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/04 12:16:42 by vcharles          #+#    #+#             */
-/*   Updated: 2016/03/13 02:38:24 by ppoinot          ###   ########.fr       */
+/*   Created: 2016/02/04 12:16:42 by ppoinot           #+#    #+#             */
+/*   Updated: 2016/03/15 00:00:58 by ppoinot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*gnl_realloc(char *buff)
+{
+	char	*sentence;
+	size_t		i;
+	size_t		n;
+
+
+	return (sentence);
+}
+
 int		get_next_line(int const fd, char **line)
 {
-	static t_gnl	*sentence;
+	static t_gnl	*static_struct;
+	t_gnl			*current_struct;
 	int				i;
 	size_t			nb_oc_lu;
 
-	if (!(sentence = (char*)malloc(sizeof(char) * BUFF_SIZE)))
+	if (!(current_struct->sentence = (char*)malloc(sizeof(char) * BUFF_SIZE)))
 		return (0);
-	if ((nb_oc_lu = read(int fd, void *sentence, size_t BUFF_SIZE)) != -1)
-		&& (nb_oc_lu != 0)
+	if ((nb_oc_lu = read(fd, current_struct->sentence, BUFF_SIZE)) != -1 ||
+		(nb_oc_lu != 0))
 	{
-		line += 1;
+		current_struct->fd = fd;
+		current_struct->line += 1;
+		
 	}
 	return (0);
 }
