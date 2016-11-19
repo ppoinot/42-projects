3c7971237b2cbb0092d5a2f62f4552827c0655a5
Paul-aurian POINOT <ppoinot@e1r5p23.42.fr>
2016-11-19 12:25:34 +0100

    32 de buffsize

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ppoinot <ppoinot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 12:06:05 by ppoinot           #+#    #+#             */
-/*   Updated: 2016/11/19 12:15:08 by ppoinot          ###   ########.fr       */
+/*   Updated: 2016/11/19 12:25:16 by ppoinot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE	1
+# define BUFF_SIZE	32
 
 # include <stdio.h>
 # include <fcntl.h>
