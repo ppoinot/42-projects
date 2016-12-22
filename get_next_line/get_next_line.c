a3850f5594c84e139b336f5de2af22e508c5ebab
Paul-Aurian POINOT <paulaurianpoinot@MacBook-Pro-de-Paul-Aurian.local>
2016-12-22 13:32:17 +0100

    No Multi-FD

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,89 +5,71 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ppoinot <ppoinot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/30 15:39:02 by ppoinot           #+#    #+#             */
-/*   Updated: 2016/12/05 18:18:25 by ppoinot          ###   ########.fr       */
+/*   Created: 2013/12/04 16:30:48 by ppoinot           #+#    #+#             */
+/*   Updated: 2014/03/27 18:42:09 by ppoinot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <fcntl.h>
-#include <stdio.h>
 
-char	*ft_swapping(char *s, int i)
+static void		ft_return(char **line, char **stce)
 {
-	char	*swap;
+	size_t		len;
+	char		*tmp;
 
-	swap = ft_strdup(s + i + 1);
-	free(s);
-	return (swap);
+	len = ft_strchr(*stce, '\n') - *stce;
+	tmp = *stce;
+	*line = ft_strsub(tmp, 0, len);
+	*stce = ft_strdup(tmp + len + 1);
+	if ((*stce)[0] == '\0')
+		ft_strdel(stce);
+	free(tmp);
 }
 
-t_gnl	*resolve(t_gnl **gnl, char ***line)
+static int		ft_end_of_file(char **line, char **stce)
 {
-	int		nb;
-
-	if ((*gnl)->ret)
+	if (!(*stce)[0])
 	{
-		(*gnl)->string = ft_strjoin((*gnl)->string, (*gnl)->buffer);
-		free((*gnl)->buffer);
-		nb = ft_count((*gnl)->string, '\n');
-		if (ft_strchr((*gnl)->string, '\n'))
-		{
-			if (nb < (*gnl)->ret)
-				(*gnl)->string = ft_swapping((*gnl)->string, nb);
-		}
-		**line = ft_strsub((*gnl)->string, 1, nb);
+		ft_strdel(stce);
+		return (0);
 	}
-	return (*gnl);
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	static t_gnl	*gnl = NULL;
-
-	if (BUFF_SIZE < 1 || !line)
-		return (-1);
-	if (!(gnl = (t_gnl*)malloc(sizeof(gnl)))
-			|| !(gnl->string = ft_strnew(BUFF_SIZE + 1))
-			|| !(gnl->buffer = ft_strnew(BUFF_SIZE + 1)))
-		return (-1);
-	gnl->ret = 1;
-	while (gnl->ret != 0 && !ft_strchr(gnl->string, '\n'))
-	{
-		if ((gnl->ret = read(fd, gnl->buffer, BUFF_SIZE)) < 0)
-			return (-1);
-		gnl = resolve(&gnl, &line);
-	}
-	if (gnl->ret != 0)
-		return (1);
-	free(gnl);
-	return (0);
-}
-
-/*int		main(void)
-{
-	int		fd;
-	char	*line;
-	int		i;
-
-	fd = open("test", O_RDONLY);
-	if (fd == -1)
-	{
-		ft_putstr("open() error");
-		return (1);
-	}
-	while ((i = get_next_line((int const)fd, &line)) > 0)
-	{
-		ft_putendl(line);
-		if (ft_strcmp(line, "aaa") == 0)
-			ft_putendl("OK");
-		else
-			ft_putendl("KO");
-		free(line);
-	}
-	close(fd);
+	*line = ft_strdup(*stce);
+	ft_strdel(stce);
 	return (1);
-}*/
+}
+
+static int		ft_error(char **stce)
+{
+	if (*stce)
+		ft_strdel(stce);
+	return (-1);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	char		buff[BUFF_SIZE + 1];
+	int			ret;
+	static char	*stce = NULL;
+	char		*tmp;
+
+	ret = 0;
+	if (fd == -1 || BUFF_SIZE <= 0)
+		return (ft_error(&stce));
+	if (stce == NULL)
+		stce = ft_strnew(BUFF_SIZE + 1);
+	while (stce != NULL
+			&& ft_strchr(stce, '\n') == NULL
+			&& ((ret = read(fd, buff, BUFF_SIZE)) > 0))
+	{
+		buff[ret] = '\0';
+		tmp = stce;
+		stce = ft_strjoin(tmp, buff);
+		free(tmp);
+	}
+	if (ret == -1 || stce == NULL)
+		return (-1);
+	if (ret == 0 && ft_strchr(stce, '\n') == NULL)
+		return (ft_end_of_file(line, &stce));
+	ft_return(line, &(stce));
+	return (1);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,23 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ppoinot <ppoinot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/12 11:00:42 by ppoinot           #+#    #+#             */
-/*   Updated: 2016/12/05 16:57:30 by ppoinot          ###   ########.fr       */
+/*   Created: 2013/12/04 16:30:48 by ppoinot           #+#    #+#             */
+/*   Updated: 2014/03/27 18:42:09 by ppoinot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <string.h>
 # include "./libft/libft.h"
 
-typedef struct		s_gnl
-{
-	char		*string;
-	char		*buffer;
-	int			ret;
-}					t_gnl;
+# define BUFF_SIZE 32
 
-int					get_next_line(const int fd, char **line);
+int	get_next_line(int fd, char **line);
 
 #endif
