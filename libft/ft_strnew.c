deaca70655b4ab0850a294faf1bcbf6f62800927
Paul-Aurian POINOT <paulaurianpoinot@MacBook-Pro-de-Paul-Aurian.local>
2016-12-24 17:45:44 +0100

    optimisation de strmapi & strnew

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -16,8 +16,7 @@ char	*ft_strnew(size_t size)
 {
 	char	*str;
 
-	str = (char*)malloc(sizeof(char) * size + 1);
-	if (str == NULL)
+	if (!(str = (char*)malloc(sizeof(char) * size + 1)))
 		return (NULL);
 	ft_bzero(str, size + 1);
 	return (str);
