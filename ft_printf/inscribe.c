19a353f5ae2f8c274a53e1d50b9e127f543115e1
Paul-Aurian POINOT <paulaurianpoinot@MacBook-Pro-de-Paul-Aurian.local>
2017-01-29 10:43:04 +0100

    change name of display.c to inscribe.c

--- ft_printf/inscribe.c
+++ ft_printf/inscribe.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   display .c                                         :+:      :+:    :+:   */
+/*   inscribe .c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ppoinot <ppoinot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/16 03:19:22 by ppoinot           #+#    #+#             */
-/*   Updated: 2016/12/16 03:00:10 by ppoinot          ###   ########.fr       */
+/*   Created: 2017/01/05 17:07:00 by ppoinot           #+#    #+#             */
+/*   Updated: 2017/01/17 18:04:04 by ppoinot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 
 void	inscribe_final(t_flags *flag, va_list *aprtf, t_info *list)
 {
-	if (is_a_conversion_specifier(flag->type))
+	if (is_a_conversion_specifier(flag->conv_spe))
 	{
-		/*if (is_an_alphabetic_flag(flag->type))
-			// display_alpha_var(flag, aprtf, list);
+		if (is_an_alphabetic_flag(flag->conv_spe))
+			display_alpha_var(flag, aprtf, list);
 		else
-			//display_numeric_var(flag, aprtf, list);*/
+			//display_numeric_var(flag, aprtf, list);*/ 
 	}
+}
+
+char	*add_to_format(char *str)
+{
+	char	*cur_arg;
+
+	cur_arg = va_arg(aprtf, char*);
+	str = ft_printf_strjoin();
+	return (str);
 }
 
 char	*flag_found(char *str, va_list *aprtf, t_info *list)
@@ -29,8 +38,8 @@ char	*flag_found(char *str, va_list *aprtf, t_info *list)
 
 	flag = init_flags();
 	str = ft_printf_checkflag(str, aprtf, list, *flag);
-	if (flag->type)
-		//print_final(flag, aprtf, list);
+	if (flag->conv_spe)
+		inscribe_final(flag, aprtf, list);
 	free(flag);
 	return (str);
 }
